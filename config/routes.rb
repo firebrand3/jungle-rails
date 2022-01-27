039ae14722eecd84ce94e5b0555227fc4a159ccd
Asim <asim.awan@outlook.com>
2022-01-27 01:27:25 +0000

    Add admin/categories route

--- config/routes.rb
+++ config/routes.rb
@@ -16,8 +16,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
-  
+
   # get '/about' => 'about#index'
   resources :about, only: [:index]
 
