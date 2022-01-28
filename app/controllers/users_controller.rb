11215753b156b17c222ccdecd2d252d58d6acad3
Asim <asim.awan@outlook.com>
2022-01-28 04:04:16 +0000

    refactor to include last name in user-params method and fix declaration error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,19 +5,24 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
-  if user.save
-    session[:user_id] = user.id
-    redirect_to '/'
-  else
-    redirect_to '/signup'
-  end   
+      if user.save
+      session[:user_id] = user.id
+      redirect_to '/'
+      else
+      flash[:errors] = user.errors.full_messages
+      redirect_to '/signup'
+    end   
+  end
 
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
+  params.require(:user).permit(
+    :first_name,
+    :last_name,
+    :email,
+    :password,
+    :password_confirmation)  
   end
 
 end
